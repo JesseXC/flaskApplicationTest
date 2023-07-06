f58b90cf241b1040f2d36e0b697b595de73db0b7
Jesse Cruz <jcruz2@oberlin.edu>
2023-07-06 16:52:07 +0000

    app chagne for git post merge webhook

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for, flash, redirect
+import git
+from flask import Flask, render_template, url_for, flash, redirect,request
 from forms import RegistrationForm
 from flask_behind_proxy import FlaskBehindProxy
+
+
 app = Flask(__name__)
 proxied = FlaskBehindProxy(app)  ## add this line
 app.config['SECRET_KEY'] = '7c3c11f178e88856343c5d14a35a1203'
@@ -21,5 +24,15 @@ def register():
         return redirect(url_for('home')) # if so - send to home page
     return render_template('register.html', title='Register', form=form)
     
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/jcruz2/flaskApplicationTest')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
